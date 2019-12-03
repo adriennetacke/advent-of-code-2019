0bf6ab0771741d206c1e150120561b0f235955a0
Adrienne Tacke <adrienne.tacke@gmail.com>
2019-12-03 01:05:17 -0800

    docs: add day 3 progress

--- day-3-crossed-wires/test.js
+++ day-3-crossed-wires/test.js
@@ -27,18 +27,19 @@ describe('Part One Crossed Wires', () => {
   });
 });
 
-describe('Part Two Crossed Wires', () => {
-  it('should return 610 when input is R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83', () => {
-    let input = `R75,D30,R83,U83,L12,D49,R71,U7,L72
-    U62,R66,U55,R34,D71,R55,D58,R83`;
-    assert.deepStrictEqual(minimalSteps(input), 610);
-  });
-});
+// In progress
+// describe('Part Two Crossed Wires', () => {
+//   it('should return 610 when input is R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83', () => {
+//     let input = `R75,D30,R83,U83,L12,D49,R71,U7,L72
+//     U62,R66,U55,R34,D71,R55,D58,R83`;
+//     assert.deepStrictEqual(minimalSteps(input), 610);
+//   });
+// });
 
-describe('Part Two Crossed Wires', () => {
-  it('should return 410 when input is R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7', () => {
-    let input = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`;
-    assert.deepStrictEqual(minimalSteps(input), 410);
-  });
-});
+// describe('Part Two Crossed Wires', () => {
+//   it('should return 410 when input is R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7', () => {
+//     let input = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+//     U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`;
+//     assert.deepStrictEqual(minimalSteps(input), 410);
+//   });
+// });
